eadb5ead1cfc635564dac913cb152247efb9c38e
Jennifer Lynn Smith <jennifer@rocktownagency.com>
2023-11-19 13:21:45 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// I need to remove the main element.
+var mainElement = document.getElementById('main');
+mainElement.remove();
+
+// Now, I'm going to create a new h1 element.
+var newHeader = document.createElement('h1');
+
+// I will give this h1 element an id of 'victory'.
+newHeader.id = 'victory';
+
+// I want this h1 element to say that I am the champion.
+newHeader.textContent = 'John Doe is the champion'; // I'll replace 'John Doe' with my name.
+
+// Finally, I'm going to add this new h1 element to my web page.
+document.body.appendChild(newHeader);
